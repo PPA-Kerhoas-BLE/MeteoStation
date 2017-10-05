0747f67499c68b5b99d3a523aa3ff325a73e21e9
Unknown <phfaverop@gmail.com>
2017-10-05 03:50:55 +0200

    iOS application added

---
+++ BLE_NUCLEO_iOS/BLE_NUCLEO_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLE_NUCLEO_iOS
+//
+//  Created by Paulo Henrique Favero Pereira on 05/10/2017.
+//  Copyright © 2017 Paulo Henrique Favero Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
